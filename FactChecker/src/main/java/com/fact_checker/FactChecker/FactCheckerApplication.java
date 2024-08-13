975c495f1a6ce4ac5e0f0ee14040fa834185ad2f
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-08-13 22:23:20 +0100

    Create skeleton spring project

---
+++ FactChecker/src/main/java/com/fact_checker/FactChecker/FactCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.fact_checker.FactChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactCheckerApplication.class, args);
+	}
+
+}
---
+++ FactChecker/src/test/java/com/fact_checker/FactChecker/FactCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fact_checker.FactChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
